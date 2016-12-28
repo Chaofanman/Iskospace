6c91f4d8e143324220719feb033c2ee7378f2363
Gabriel Patron <gchase.patron@gmail.com>
2016-12-28 20:22:16 +0800

    Migrations and models for posts and users done

---
+++ web/models/post.ex
@@ -0,0 +1,17 @@
+defmodule Iskospace.Post do
+	use Iskospace.Web, :model
+
+	schema "posts" do
+		field :title, :string
+		field :body, :string
+		belongs_to :user, Iskospace.User
+
+		timestamps()
+	end
+
+	def changeset(struct, params \\ %{}) do
+		struct
+		|> cast(params, [:title, :body])
+		|> validate_required([:title, :body])
+	end
+end
---
+++ web/models/user.ex
@@ -0,0 +1,19 @@
+defmodule Iskospace.User do
+	use Iskospace.Web, :model
+
+	schema "users" do
+		field :first_name, :string
+		field :last_name, :string
+		field :email, :string
+		field :password, :string
+		has_many :posts, Iskospace.Post
+
+		timestamps()
+	end
+
+	def changeset(struct, params \\%{}) do
+		struct
+		|> cast(params, [:first_name, :last_name, :email, :password])
+		|> validate_required([:first_name, :last_name, :email, :password])
+	end
+end
