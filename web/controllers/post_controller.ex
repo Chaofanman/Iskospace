6a01a98c889daba4b66a369d836a1a5178ca4bf4
Gabriel Patron <gchase.patron@gmail.com>
2016-12-30 21:26:36 +0800

    Post controller and some templates done

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -2,12 +2,21 @@ defmodule Iskospace.PostController do
 	use Iskospace.Web, :controller
 
 	alias Iskospace.Post
+	alias Iskospace.User
 
-	# plug Iskospace.Plugs.AuthorizeUser
+	plug :scrub_params, "post" when action in [:create, :update]
 
-	def index(conn, _params) do
-		posts = Repo.all(assoc(conn.assigns[:user], :posts))
-		render(conn, "index.html", posts: posts)
+	def index(conn, %{"user_id" => user_id}) do
+		posts_owner = Repo.get!(User, user_id |> String.to_integer)
+		posts = Repo.all(assoc(posts_owner, :posts))
+		render(conn, "index.html", posts: posts, posts_owner: posts_owner)
+	end
+
+	def show(conn, %{"id" => post_id}) do
+		post = Repo.get!(Post, post_id)
+		|> Repo.preload(:user)
+		IO.inspect post
+		render(conn, "show.html", post: post)
 	end
 
 	def new(conn, _params) do
@@ -31,4 +40,24 @@ defmodule Iskospace.PostController do
 				render(conn, "new.html", changeset: changeset)
 		end
 	end	
+
+	def edit(conn, %{"id" => post_id}) do
+		post = Repo.get!(assoc(conn.assigns[:user], :posts), post_id)	
+		changeset = Post.changeset(post)
+		render(conn, "edit.html", changeset: changeset, post: post)
+	end
+
+	def update(conn, %{"id" => post_id, "post" => post_params}) do
+		old_post = Repo.get!(assoc(conn.assigns[:user], :posts), post_id)		
+		changeset = Post.changeset(old_post, post_params)
+
+		case Repo.update(changeset) do
+			{:ok, new_post} ->
+				conn 
+				|> put_flash(:info, "Successfully updated post")
+				|> redirect(to: user_post_path(conn, :show, conn.assigns[:user], new_post))
+			{:error, changeset} -> 
+				render(conn, "edit.html", changeset: changeset, post: old_post)
+		end
+	end
 end
