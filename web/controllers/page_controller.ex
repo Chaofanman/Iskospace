d7d6d36c76cde5d19ade4a40ebd6427f147258a9
Gabriel Patron <gchase.patron@gmail.com>
2016-12-31 13:00:21 +0800

    Loads all posts from database to page index

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule Iskospace.PageController do
   use Iskospace.Web, :controller
 
+  alias Iskospace.Post
+
   def index(conn, _params) do
-    render conn, "index.html"
+    posts = Repo.all(from p in Post, preload: [:user])
+		render(conn, "index.html", posts: posts)
   end
 end
