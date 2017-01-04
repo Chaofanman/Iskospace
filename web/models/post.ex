76abd5b476d7be3fb84c8fffb0a6682797d9881e
Gabriel Patron <gchase.patron@gmail.com>
2017-01-04 16:01:56 +0800

    Finish get_tags to save to database with post_id

--- web/models/post.ex
+++ web/models/post.ex
@@ -8,11 +8,29 @@ defmodule Iskospace.Post do
 		has_many :comments, Iskospace.Comment
 
 		timestamps()
+
+		field :tags, :string, virtual: true
 	end
 
 	def changeset(struct, params \\ %{}) do
 		struct
-		|> cast(params, [:title, :body])
+		|> cast(params, [:title, :body, :tags])
 		|> validate_required([:title, :body])
+		|> get_tags
 	end
+
+	defp get_tags(changeset) do
+		tags = get_change(changeset, :tags)
+			|> to_string
+			|> String.split(",")
+			|> Enum.map(&save_to_database/1)
+		changeset
+	end
+
+	defp save_to_database(tag) do
+		tag 
+		|> String.trim(" ")
+		|> IO.inspect 
+	end
+
 end
